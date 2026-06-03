e6e06120de0ce408862712e74737bd6ba844e66b
Saint Blanc <service@inexthome.com>
2026-06-03 14:00:13 -0400

    Align Capacitor app id with iOS bundle

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.loombus.app",
+  appId: "com.loombus.mobile",
   appName: "Loombus",
   webDir: "public",
   server: {
