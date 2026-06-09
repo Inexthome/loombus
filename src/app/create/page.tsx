14efa28359cd082394cbdaca008fbd55123e0f3e
Saint Blanc <service@inexthome.com>
2026-06-09 01:48:47 -0400

    Keep quotes readable in discussion text

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -419,8 +419,7 @@ function escapeLimitedHtml(value: string) {
   return value
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;");
+    .replace(/>/g, "&gt;");
 }
 
 function hasLimitedFormattingHtml(value: string) {
--- src/app/discussions/[id]/page.tsx
+++ src/app/discussions/[id]/page.tsx
@@ -315,8 +315,7 @@ function escapeLimitedHtml(value: string) {
   return value
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;");
+    .replace(/>/g, "&gt;");
 }
 
 function hasLimitedFormattingHtml(value: string) {
