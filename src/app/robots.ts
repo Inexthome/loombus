f8307f49221ae3ad9d34ada8c45787dbe8e1e108
Saint Blanc <service@inexthome.com>
2026-06-19 20:01:52 -0400

    Noindex public safety page

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -11,7 +11,6 @@ export default function robots(): MetadataRoute.Robots {
           "/",
           "/about",
           "/download",
-          "/safety",
           "/guidelines",
           "/privacy",
           "/terms",
