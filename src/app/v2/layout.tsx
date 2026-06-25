10396d0e520143cfb7d7ebe569bc1bcdbe78c615
Inexthome <service@inexthome.com>
2026-06-25 16:34:44 -0400

    Wire V2 create review action

--- src/app/v2/layout.tsx
+++ src/app/v2/layout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from "react";
 import { V2ShellLinkRouter } from "./v2-shell-link-router";
+import { V2CreateReviewAction } from "./create/v2-create-review-action";
 
 export default function V2Layout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
       <V2ShellLinkRouter />
+      <V2CreateReviewAction />
     </>
   );
 }
