a0ef62b9d4dea880c37d49bb78a0057acffeac44
Inexthome <service@inexthome.com>
2026-06-27 03:27:41 -0400

    Add V2 people live read-only page

---
+++ src/app/v2/people/page.tsx
@@ -0,0 +1,5 @@
+import { V2LiveReadOnlySectionPage } from "../v2-live-readonly-section-page";
+
+export default function V2PeoplePage() {
+  return <V2LiveReadOnlySectionPage sectionSlug="people" />;
+}
