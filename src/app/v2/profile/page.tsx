e8c74a847e1a15a270985d4e50781ae5d573a83f
Inexthome <service@inexthome.com>
2026-06-27 03:36:08 -0400

    Add V2 profile live read-only page

---
+++ src/app/v2/profile/page.tsx
@@ -0,0 +1,5 @@
+import { V2LiveReadOnlySectionPage } from "../v2-live-readonly-section-page";
+
+export default function V2ProfilePage() {
+  return <V2LiveReadOnlySectionPage sectionSlug="profile" />;
+}
