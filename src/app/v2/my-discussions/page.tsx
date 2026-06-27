df06c650ef8c39729eec3fef4cc7955af5b34865
Inexthome <service@inexthome.com>
2026-06-27 03:33:39 -0400

    Add V2 my discussions live read-only page

---
+++ src/app/v2/my-discussions/page.tsx
@@ -0,0 +1,5 @@
+import { V2LiveReadOnlySectionPage } from "../v2-live-readonly-section-page";
+
+export default function V2MyDiscussionsPage() {
+  return <V2LiveReadOnlySectionPage sectionSlug="my-discussions" />;
+}
