340158b6bce6bbd3f804da2774e6d679963c98bb
Inexthome <service@inexthome.com>
2026-06-29 17:06:07 -0400

    Wire Create public polish helper

--- src/app/v2/create/page.tsx
+++ src/app/v2/create/page.tsx
@@ -1,5 +1,11 @@
 import V2CreateClientPage from "./client-page";
+import CreatePublicPolish from "./create-public-polish";
 
 export default function V2CreatePage() {
-  return <V2CreateClientPage />;
+  return (
+    <>
+      <CreatePublicPolish />
+      <V2CreateClientPage />
+    </>
+  );
 }
