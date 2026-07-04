d3ccad416ac6f81106cd91412201baf12e849afa
Inexthome <service@inexthome.com>
2026-07-04 14:48:48 -0400

    Load V2 Stickies contrast styles

---
+++ src/app/v2/stickies/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "../../stickies/stickies-contrast.css";
+
+export default function V2StickiesLayout({ children }: { children: ReactNode }) {
+  return children;
+}
