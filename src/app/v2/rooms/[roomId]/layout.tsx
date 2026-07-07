d9bafb9943a1f2c9b7390e26d24a9484d2f6569d
Inexthome <service@inexthome.com>
2026-07-07 13:15:41 -0400

    Use Room Menu only on room hub

--- src/app/v2/rooms/[roomId]/layout.tsx
+++ src/app/v2/rooms/[roomId]/layout.tsx
@@ -1,6 +1,5 @@
 import { RoomPlanMenu } from "./room-plan-menu";
 import { RoomPreferencesEnforcer } from "./room-preferences-enforcer";
-import { RoomQuickActions } from "./room-quick-actions";
 
 export default async function V2RoomLayout({ children, params }: { children: React.ReactNode; params: Promise<{ roomId: string }> }) {
   const { roomId } = await params;
@@ -9,7 +8,6 @@ export default async function V2RoomLayout({ children, params }: { children: Rea
     <>
       {children}
       <RoomPlanMenu roomId={roomId} />
-      <RoomQuickActions roomId={roomId} />
       <RoomPreferencesEnforcer roomId={roomId} />
     </>
   );
