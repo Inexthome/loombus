db59e533542542356d20ffc4671109cb7164c1c1
Inexthome <service@inexthome.com>
2026-07-08 13:29:56 -0400

    Render restored room detail right rail

--- src/app/v2/rooms/[roomId]/page.tsx
+++ src/app/v2/rooms/[roomId]/page.tsx
@@ -1,5 +1,11 @@
 import RoomDetailClient from "./room-detail-client";
+import RoomDetailRightRail from "./room-detail-right-rail";
 
 export default function V2RoomDetailPage() {
-  return <RoomDetailClient />;
+  return (
+    <>
+      <RoomDetailClient />
+      <RoomDetailRightRail />
+    </>
+  );
 }
