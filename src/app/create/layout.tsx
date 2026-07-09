e3f8cf160629095d4af768301e302d9dfb6e4882
Inexthome <service@inexthome.com>
2026-07-09 14:22:53 -0400

    Remove old Create shell helpers

--- src/app/create/layout.tsx
+++ src/app/create/layout.tsx
@@ -1,16 +1,7 @@
-import { CreateEditHydrator } from "@/components/create-edit-hydrator";
-import { CreateSignalGuide } from "@/components/create-signal-guide";
-
 export default function CreateLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <>
-      {children}
-      <CreateSignalGuide />
-      <CreateEditHydrator />
-    </>
-  );
+  return <>{children}</>;
 }
