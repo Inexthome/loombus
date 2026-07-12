956d888bb8b22478cd9502e1bccd94fb69cdecda
Inexthome <service@inexthome.com>
2026-07-12 01:54:02 -0400

    Add topic-filtered Signal route

---
+++ src/app/topics/[topic]/page.tsx
@@ -0,0 +1,8 @@
+import TopicSignalClient from "./topic-signal-client";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function TopicSignalPage() {
+  return <TopicSignalClient />;
+}
