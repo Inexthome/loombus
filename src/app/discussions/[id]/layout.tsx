a24d213e9a36f93f4c5f166b4d948a304d7ae3f4
Inexthome <service@inexthome.com>
2026-07-12 04:34:23 -0400

    Wrap canonical discussion detail experience

--- src/app/discussions/[id]/layout.tsx
+++ src/app/discussions/[id]/layout.tsx
@@ -1,4 +1,6 @@
 import { ReplySignalGuide } from "@/components/reply-signal-guide";
+import DiscussionDetailShare from "./discussion-detail-share";
+import "./discussion-detail-v2.css";
 
 export default function DiscussionDetailLayout({
   children,
@@ -6,9 +8,10 @@ export default function DiscussionDetailLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <div className="discussion-detail-route">
       {children}
+      <DiscussionDetailShare />
       <ReplySignalGuide />
-    </>
+    </div>
   );
 }
