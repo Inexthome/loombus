e21e3dc19652f88e62c8970a6566df7b87cd71b0
Inexthome <service@inexthome.com>
2026-07-15 23:40:49 -0400

    Wire guarded create publishing

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,5 +1,10 @@
+import { CreatePublishGuard } from "@/components/create-publish-guard";
 import CreateV2ClientPage from "./create-v2-client-page";
 
 export default function CreatePage() {
-  return <CreateV2ClientPage />;
+  return (
+    <CreatePublishGuard>
+      <CreateV2ClientPage />
+    </CreatePublishGuard>
+  );
 }
