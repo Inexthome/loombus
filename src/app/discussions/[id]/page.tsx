9691c855a602b8d2862ad10001c86d9e857f73f7
Inexthome <saint@loombus.com>
2026-07-20 19:21:11 -0400

    Restore Discussion detail safety and management actions

--- src/app/discussions/[id]/page.tsx
+++ src/app/discussions/[id]/page.tsx
@@ -1,5 +1,11 @@
+import DiscussionDetailActionsLayer from "./discussion-detail-actions-layer";
 import DiscussionDetailV2Client from "./discussion-detail-v2-client";
 
 export default function DiscussionPage() {
-  return <DiscussionDetailV2Client />;
+  return (
+    <>
+      <DiscussionDetailV2Client />
+      <DiscussionDetailActionsLayer />
+    </>
+  );
 }
