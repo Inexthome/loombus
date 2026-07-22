d7ad55d9fe4eeab4ba8454d7ca0f47a91f618088
Inexthome <saint@loombus.com>
2026-07-22 00:16:32 -0400

    Mount Create audience controls

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,3 +1,4 @@
+import { CreateDiscussionAudienceBridge } from "@/components/create-discussion-audience-bridge";
 import { CreateDiscussionRefinements } from "@/components/create-discussion-refinements";
 import { CreatePublishGuard } from "@/components/create-publish-guard";
 import CreateV2ClientPage from "./create-v2-client-page";
@@ -6,6 +7,7 @@ export default function CreatePage() {
   return (
     <CreatePublishGuard>
       <CreateDiscussionRefinements />
+      <CreateDiscussionAudienceBridge />
       <CreateV2ClientPage />
     </CreatePublishGuard>
   );
