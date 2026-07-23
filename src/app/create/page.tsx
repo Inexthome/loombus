02e63f8659eed623074ce2c9dc9a9c1833aea9d7
Inexthome <saint@loombus.com>
2026-07-23 00:42:15 -0400

    Use native shared create composer

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,18 +1,10 @@
-import { CreateDiscussionAudiencePolicyGuard } from "@/components/create-discussion-audience-policy-guard";
-import { CreateDiscussionRefinements } from "@/components/create-discussion-refinements";
-import { CreateMobileComposerAdapter } from "@/components/create-mobile-composer-adapter";
-import { CreateMobileComposerCorrections } from "@/components/create-mobile-composer-corrections";
 import { CreatePublishGuard } from "@/components/create-publish-guard";
 import CreateV2ClientPage from "./create-v2-client-page";
 
 export default function CreatePage() {
   return (
     <CreatePublishGuard>
-      <CreateDiscussionRefinements />
-      <CreateDiscussionAudiencePolicyGuard />
       <CreateV2ClientPage />
-      <CreateMobileComposerAdapter />
-      <CreateMobileComposerCorrections />
     </CreatePublishGuard>
   );
 }
