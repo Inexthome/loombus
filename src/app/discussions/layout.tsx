caa39c70fd28859474c7b36263260f8e86bffdb1
Inexthome <saint@loombus.com>
2026-07-22 23:44:38 -0400

    Mount discussions create composer preview

--- src/app/discussions/layout.tsx
+++ src/app/discussions/layout.tsx
@@ -1,9 +1,11 @@
 import { DiscussionAutoLinker } from "@/components/discussion-auto-linker";
 import { DiscussionFeedRefinements } from "@/components/discussion-feed-refinements";
 import { DiscussionPublishToast } from "@/components/discussion-publish-toast";
+import { DiscussionsCreateComposerBridge } from "@/components/discussions-create-composer-bridge";
 import { DiscussionVideoAutoplay } from "@/components/discussion-video-autoplay";
 import "./discussion-feed-media.css";
 import "./discussion-feed-refinements.css";
+import "./discussions-create-modal.css";
 
 export default function DiscussionsLayout({
   children,
@@ -16,6 +18,7 @@ export default function DiscussionsLayout({
       <DiscussionFeedRefinements />
       <DiscussionPublishToast />
       <DiscussionVideoAutoplay />
+      <DiscussionsCreateComposerBridge />
       {children}
     </div>
   );
