b29092a156a04d573c5de2b615d0c9661e513b53
Inexthome <saint@loombus.com>
2026-07-26 17:22:36 -0400

    Connect Featured Signal attachment previews

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,12 @@
+import HomeFeaturedSignalAttachment from "@/components/home-featured-signal-attachment";
 import HomeV2ClientPage from "./home-v2-client-page";
+import "./home-featured-signal-attachment.css";
 
 export default function HomePage() {
-  return <HomeV2ClientPage />;
+  return (
+    <>
+      <HomeV2ClientPage />
+      <HomeFeaturedSignalAttachment />
+    </>
+  );
 }
