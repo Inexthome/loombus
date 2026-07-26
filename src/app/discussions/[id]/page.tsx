19bdb74c87cf2ef6d0ddef956c7549b34f4f4b86
Inexthome <saint@loombus.com>
2026-07-26 17:55:25 -0400

    Mount discussion reader identities panel

--- src/app/discussions/[id]/page.tsx
+++ src/app/discussions/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { DiscussionAudienceDetailBadge } from "@/components/discussion-audience-detail-badge";
+import { DiscussionViewersPanel } from "@/components/discussion-viewers-panel";
 import DiscussionDetailActionsLayer from "./discussion-detail-actions-layer";
 import DiscussionDetailV2Client from "./discussion-detail-v2-client";
 
@@ -6,6 +7,7 @@ export default function DiscussionPage() {
   return (
     <>
       <DiscussionDetailV2Client />
+      <DiscussionViewersPanel />
       <DiscussionAudienceDetailBadge />
       <DiscussionDetailActionsLayer />
     </>
