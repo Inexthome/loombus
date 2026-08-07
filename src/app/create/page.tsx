6a045145bfa37905a53187236833a66123cb4117
Inexthome <saint@loombus.com>
2026-08-07 01:16:57 -0400

    Show profile completion notice in create

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,9 +1,11 @@
+import { CreateProfileCompletionNotice } from "@/components/create-profile-completion-notice";
 import { CreatePublishGuard } from "@/components/create-publish-guard";
 import CreateV2ClientPage from "./create-v2-client-page";
 
 export default function CreatePage() {
   return (
     <CreatePublishGuard>
+      <CreateProfileCompletionNotice />
       <CreateV2ClientPage />
     </CreatePublishGuard>
   );
