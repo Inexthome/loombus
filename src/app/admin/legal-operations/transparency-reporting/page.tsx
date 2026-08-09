dfa63361b0be2c8732a5eae76b476472b5434025
Inexthome <saint@loombus.com>
2026-08-09 02:48:55 -0400

    Add transparency reporting admin page

---
+++ src/app/admin/legal-operations/transparency-reporting/page.tsx
@@ -0,0 +1,10 @@
+import TransparencyReportingClient from "./transparency-reporting-client";
+
+export const metadata = {
+  title: "Transparency Reporting | Loombus Admin",
+  robots: { index: false, follow: false },
+};
+
+export default function TransparencyReportingPage() {
+  return <TransparencyReportingClient />;
+}
