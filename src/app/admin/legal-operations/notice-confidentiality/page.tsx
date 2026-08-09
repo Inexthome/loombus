b6a0fce313c0fc748b0d297625ee5adec9ff7bb8
Inexthome <saint@loombus.com>
2026-08-09 03:45:24 -0400

    Add notice confidentiality page

---
+++ src/app/admin/legal-operations/notice-confidentiality/page.tsx
@@ -0,0 +1,7 @@
+import NoticeConfidentialityClient from "./notice-confidentiality-client";
+
+export const dynamic = "force-dynamic";
+
+export default function NoticeConfidentialityPage() {
+  return <NoticeConfidentialityClient />;
+}
