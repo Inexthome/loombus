02d5ed8b9dc012a43630f1e58a2e6e7bd7cfab2d
Inexthome <saint@loombus.com>
2026-08-17 05:54:41 -0400

    Add Discuss Passage page

---
+++ src/app/library/discuss-passage/page.tsx
@@ -0,0 +1,5 @@
+import { LibraryDiscussPassageComposer } from "@/components/library/library-discuss-passage-composer";
+
+export default function LibraryDiscussPassagePage() {
+  return <LibraryDiscussPassageComposer />;
+}
