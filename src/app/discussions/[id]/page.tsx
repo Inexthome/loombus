f07c58d9761fb6d6c21b8d4d77b3812c3f70b3be
Inexthome <saint@loombus.com>
2026-08-22 04:40:51 -0400

    Link discussions back into private Library knowledge

--- src/app/discussions/[id]/page.tsx
+++ src/app/discussions/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { DiscussionAudienceDetailBadge } from "@/components/discussion-audience-detail-badge";
 import { DiscussionViewersPanel } from "@/components/discussion-viewers-panel";
+import { DiscussionLibraryFeedbackLauncher } from "@/components/library/discussion-library-feedback-launcher";
 import "./discussion-mobile-back-navigation.css";
 import DiscussionDetailActionsLayer from "./discussion-detail-actions-layer";
 import DiscussionDetailV2Client from "./discussion-detail-v2-client";
@@ -13,6 +14,7 @@ export default function DiscussionPage() {
       <DiscussionViewersPanel />
       <DiscussionAudienceDetailBadge />
       <DiscussionDetailActionsLayer />
+      <DiscussionLibraryFeedbackLauncher />
     </>
   );
 }
