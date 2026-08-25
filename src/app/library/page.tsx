559920b0fbe6985997fefb199f0d2394864a5146
Inexthome <saint@loombus.com>
2026-08-24 23:51:19 -0400

    Wrap Library home in immersive mobile shell

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,5 +1,10 @@
 import { LibraryFunctionalSurface } from "@/components/library/library-functional-surface";
+import { LibraryMobileImmersiveShell } from "@/components/library/library-mobile-immersive-shell";
 
 export default function LibraryPage() {
-  return <LibraryFunctionalSurface />;
+  return (
+    <LibraryMobileImmersiveShell>
+      <LibraryFunctionalSurface />
+    </LibraryMobileImmersiveShell>
+  );
 }
