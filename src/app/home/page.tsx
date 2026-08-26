6ffec31da971b067f5c962e36ad6ed89f1ec7ff3
Inexthome <saint@loombus.com>
2026-08-26 04:37:14 -0400

    Polish compact Home navigation

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
 import HomeFeaturedSignalAttachment from "@/components/home-featured-signal-attachment";
 import HomeCompactClient from "./home-compact-client";
 import "./home-compact.css";
+import "./home-compact-polish.css";
 import "./home-featured-signal-attachment.css";
 
 export default function HomePage() {
