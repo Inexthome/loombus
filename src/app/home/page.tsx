8e251154d00c9fedee8636116859ab52ee3c1aee
Inexthome <saint@loombus.com>
2026-08-26 04:34:23 -0400

    Switch Home to compact feed redesign

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,12 @@
 import HomeFeaturedSignalAttachment from "@/components/home-featured-signal-attachment";
-import HomeV2ClientPage from "./home-v2-client-page";
+import HomeCompactClient from "./home-compact-client";
+import "./home-compact.css";
 import "./home-featured-signal-attachment.css";
 
 export default function HomePage() {
   return (
     <>
-      <HomeV2ClientPage />
+      <HomeCompactClient />
       <HomeFeaturedSignalAttachment />
     </>
   );
