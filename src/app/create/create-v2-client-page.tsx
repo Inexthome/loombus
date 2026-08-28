042cfa2721134290baf9e5ed588762fde1fda04a
Inexthome <saint@loombus.com>
2026-08-28 08:36:00 -0400

    Mount create info tooltips

--- src/app/create/create-v2-client-page.tsx
+++ src/app/create/create-v2-client-page.tsx
@@ -2,12 +2,14 @@
 
 import CreateDiscussionComposer from "@/components/create-discussion-composer";
 import { CreateAttachmentTriggerBridge } from "@/components/create-attachment-trigger-bridge";
+import { CreateInfoTooltips } from "@/components/create-info-tooltips";
 
 export default function CreateV2ClientPage() {
   return (
     <>
       <CreateDiscussionComposer />
       <CreateAttachmentTriggerBridge />
+      <CreateInfoTooltips />
     </>
   );
 }
