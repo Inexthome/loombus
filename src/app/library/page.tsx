c45f9da7024ab0eb62ab0f81c7980e4da840313d
Inexthome <saint@loombus.com>
2026-08-29 01:37:18 -0400

    Apply Editorial UI to Library landing

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,5 +1,10 @@
 import { LibraryFunctionalSurface } from "@/components/library/library-functional-surface";
+import "./library-editorial-home.css";
 
 export default function LibraryPage() {
-  return <LibraryFunctionalSurface />;
+  return (
+    <div data-library-editorial-home>
+      <LibraryFunctionalSurface />
+    </div>
+  );
 }
