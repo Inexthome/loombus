b77e4a9297d71efd8d3eb7f09d9314ed35c9f1b2
Inexthome <saint@loombus.com>
2026-08-31 05:53:21 -0400

    Register native Google auth plugin

--- ios/App/App/LoombusBridgeViewController.swift
+++ ios/App/App/LoombusBridgeViewController.swift
@@ -1,9 +1,11 @@
 import Capacitor
+import CapApp_SPM
 
 @objc(LoombusBridgeViewController)
 final class LoombusBridgeViewController: CAPBridgeViewController {
     override func capacitorDidLoad() {
         bridge?.registerPluginInstance(LoombusLiveUpdatesPlugin())
         bridge?.registerPluginInstance(LoombusPasswordManagerPlugin())
+        bridge?.registerPluginInstance(LoombusGoogleAuthPlugin())
     }
 }
