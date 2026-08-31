9aac6e16aba7551b891a825df6285592a1c6f8ce
Inexthome <saint@loombus.com>
2026-08-30 20:03:53 -0400

    Wire Topics Editorial styles

--- src/app/topics/page.tsx
+++ src/app/topics/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import SignalDirectoryClient from "./signal-directory-client";
+import "./topics-editorial.css";
 
 export const metadata: Metadata = {
   title: "Topics",
